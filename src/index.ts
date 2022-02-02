25a2f7e36b1a951560bbe863967f58ec74674759
panyue <90938238g@gmail.com>
2022-02-02 16:44:55 +0800

    主文件

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import dotevn from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import morgan from 'morgan';
+
+dotevn.config();
+
+const app = express();
+
+//middlewares 中间件
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+
+
+//数据库 database
+const URI = process.env.MONGODB_URL;
+mongoose.connect(URI, {
+    autoIndex: false,
+}, (err) => {
+    if(err) throw err;
+    console.log('MongoDB connected');
+});
+
+//路由 route
+app.get('/', (req, res) => {
+  res.json({
+      msg:'hello py'
+  })
+});
+
+
+//starts server
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
