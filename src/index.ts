753b8191ccca2f4759c13dd83d1e990b7acc207e
panyue <90938238g@gmail.com>
2022-02-02 17:54:29 +0800

    主入口文件

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import morgan from 'morgan';
+import routes from "./routes";
 
 dotevn.config();
 
@@ -20,20 +21,15 @@ const URI = process.env.MONGODB_URL;
 mongoose.connect(URI, {
     autoIndex: false,
 }, (err) => {
-    if(err) throw err;
+    if (err) throw err;
     console.log('MongoDB connected');
 });
 
 //路由 route
-app.get('/', (req, res) => {
-  res.json({
-      msg:'hello py'
-  })
-});
-
+app.use('/api', routes);
 
 //starts server
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
