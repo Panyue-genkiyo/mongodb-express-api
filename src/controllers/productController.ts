9c6bebf2cf4dfad6cbde3329ceb82c0da878cfc9
panyue <90938238g@gmail.com>
2022-02-02 19:23:57 +0800

    product控制器

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,33 +1,107 @@
+//引进model
+import Product from "../models/productModel";
 
 const productController = {
    getProducts: async (req, res) => {
-       res.json({
-           msg:'get all products'
-       })
+       try{
+           const products = await Product.find();
+           return res.status(200).json({
+               status:'success',
+               data: products
+           })
+       }catch (err){
+           return res.status(500).json({
+               message: err.message
+           })
+       }
    },
    getProduct: async (req, res) => {
        console.log(req.params);
-       res.json({
-           msg:`get one product by id:${req.params.id}`
-       })
+       try{
+           const product = await Product.findById(req.params.id);
+           if(!product){
+               return res.status(404).json({
+                   status: 'fail',
+                   message: 'Product not found'
+               })
+           }
+           return res.status(200).json({
+               status:'success',
+               data: product
+           });
+       }catch (err){
+           return res.status(500).json({
+               message: err.message
+           })
+       }
    },
    createProduct: async (req, res) => {
-       console.log(req.body);
-       res.json({
-           msg:'add new product'
-       })
+       try{
+           const { title, description, image, category, price } = req.body;
+           const newProduct = new Product({
+               title,
+               description,
+               image,
+               category,
+               price
+           });
+           await newProduct.save();
+           return res.status(200).json({
+               status:'success',
+               msg:'product created successfully',
+               data: newProduct
+           });
+       }catch (err){
+           return res.status(500).json({
+               message: err.message
+           })
+       }
    },
    updateProduct: async (req, res) => {
-       console.log(req.body);
-       res.json({
-           msg:`update product with id: ${req.params.id}`
-       })
+       try{
+           const { title, description, image, category, price } = req.body;
+           const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
+               title,
+               description,
+               image,
+               category,
+               price
+           }, { new: true });
+           if(!updatedProduct){
+               return res.status(404).json({
+                   status: 'fail',
+                   message: 'Product not found'
+               })
+           }
+           return res.status(200).json({
+               status:'success',
+               msg:'product updated successfully',
+               data: updatedProduct
+           });
+       }catch (err){
+           return res.status(500).json({
+               message: err.message
+           })
+       }
    },
    deleteProduct: async (req, res) => {
-       console.log(req.params);
-       res.json({
-           msg:`delete product with id: ${req.params.id}`
-       })
+       try{
+           const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+           if(!deletedProduct){
+               return res.status(404).json({
+                   status: 'fail',
+                   message: 'Product not found'
+               })
+           }
+           return res.status(200).json({
+               status:'success',
+               msg:'product deleted successfully'
+           });
+       }catch (err){
+           return res.status(500).json({
+               message: err.message
+           })
+       }
    }
 }
 
